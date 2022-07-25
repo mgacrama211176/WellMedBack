75699752910548e65cd714f0e3fa7d9274f168bc
Marlon Gacrama <mgacrama_ccs@uspf.edu.ph>
2022-07-25 19:01:39 +0800

    retry atlas

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import SearchRoute from './routes/search.js';
 //connecting to DB
 import connectdb from './connectdb.js';
 connectdb(
-  'mongodb+srv://admin:Administrator@cluster0.hxm03.mongodb.net',
+  'mongodb+srv://admin:Administrator123@cluster0.hxm03.mongodb.net/?retryWrites=true&w=majority',
   'WellMed'
 );
 
