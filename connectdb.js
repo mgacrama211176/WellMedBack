f493daab8a8001124dc37a3abe1fca5559ba48a3
Marlon Gacrama <mgacrama_ccs@uspf.edu.ph>
2022-07-25 18:51:53 +0800

    changes

--- connectdb.js
+++ connectdb.js
@@ -7,8 +7,9 @@ const { connect } = mongoose;
 export default async (host, dbName) => {
   try {
     debug('Database is connected', host, dbName);
-    console.log('Database is now Connected', dbName);
+
     await connect(host, { dbName });
+    console.log('Database is now Connected', dbName);
   } catch (e) {
     console.error('failed connecting to mongodb server')`console.error(e)`;
   }
